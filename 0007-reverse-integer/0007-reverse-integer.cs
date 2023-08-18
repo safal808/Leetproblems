8f76be7fb851fb3732b85b18198c3104c490ae2c
Safal Poudel <85111654+safal808@users.noreply.github.com>
2023-08-18 23:59:21 +0545

    Time: 13 ms (99.53%), Space: 26.7 MB (72.61%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Reverse(int x) {
+    
+        long rev=x;
+        int sum = 0;
+        
+        while (x != 0) {
+            int r = x % 10;
+            
+            if (sum > int.MaxValue / 10 || sum < int.MinValue / 10) {
+                return 0;
+            }
+            
+            sum = sum * 10 + r;
+            x = x / 10;
+        } 
+        return sum;
+    }
+}
